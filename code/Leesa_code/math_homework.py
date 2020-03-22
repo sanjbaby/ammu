7844e192ef02d06d01ef3173abfe0a91962b80cf
Sanju Baby <sanjbaby@in.ibm.com>
2020-03-22 11:18:56 +0530

    homework companion

---
+++ code/Leesa_code/math_homework.py
@@ -0,0 +1,4 @@
+problem = input("enter a math problem or 'q'to quit: ")
+while (problem != 'q'):
+    print("the answer to",problem ,"is:" , eval(problem))
+  problem = input("enter another math problem or 'q' to quit")
