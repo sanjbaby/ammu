88b27b065b230accdad116c32ae1290d4dd7b3c6
sanjbaby <53223902+sanjbaby@users.noreply.github.com>
2020-03-25 10:37:01 +0530

    weather reports for every day

---
+++ code/Leesa_code/what_to_wear.py
@@ -0,0 +1,12 @@
+rainy = input("how is the weather is it raining? (y/n)")
+cold = input ("is it cold outside? (y/n)")
+if (rainy == 'y' and cold == 'y'):
+    print("you would better stay inside with a cup of hot chocolate")
+elif (rainy == 'y'and cold  !='y'):
+    print("you can carry an umbrella with you")
+elif(rainy !='y'and cold=='y'):
+    print("you can go out with a sweater or jacket")
+elif(rainy !='y'and cold!= 'y'):
+    print("wow! that sounds like the best weather for having picnic or go out with family and friends to a the beach or zoo")
+
+
